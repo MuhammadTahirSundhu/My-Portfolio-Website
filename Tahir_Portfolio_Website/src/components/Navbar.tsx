81eec092f903e52c0e18fcaa4e62256370c9496e
MuhammadTahirSundhu <tahirsundhu8956@gmail.com>
2024-09-15 16:19:39 +0500

    Responsiveness of header section also done

--- Tahir_Portfolio_Website/src/components/Navbar.tsx
+++ Tahir_Portfolio_Website/src/components/Navbar.tsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css'; // Import the CSS file
-import { FaGithub, FaLinkedin, FaGoogle } from 'react-icons/fa'; // Import icons
-
+import { FaGithub, FaLinkedin, FaGoogle, FaBars } from 'react-icons/fa'; // Import icons
 
 const Navbar: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const navigate = useNavigate();
-    const handleNavigation = (path:string)=>{
-        console.log("clicked");
-        
+
+    const handleNavigation = (path: string) => {
         navigate(path);
-    }
+        setIsMenuOpen(false); // Close the mobile menu when a link is clicked
+    };
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className="navbar">
-            <span className="brand"><span>&#128522;</span>Mr Sundhu</span>
-            <div className="Pages">
-                <span className = "Page" onClick={()=>handleNavigation("/")} >Home</span>
-                <span className = "Page" onClick={()=>handleNavigation("/portfolio")} >Portfolio</span>
-                <span className = "Page" onClick={()=>handleNavigation("/testimonials")} >Testimonials</span>
-                <span className = "Page" onClick={()=>handleNavigation("/about")} >About</span>
-                <span className = "Page" onClick={()=>handleNavigation("/contact")} >Contact</span>
+            <span className="brand"><span>&#128522;</span> Mr Sundhu</span>
+            <div className={`Pages ${isMenuOpen ? 'open' : ''}`}>
+                <span className="Page" onClick={() => handleNavigation('/')}>Home</span>
+                <span className="Page" onClick={() => handleNavigation('/portfolio')}>Portfolio</span>
+                <span className="Page" onClick={() => handleNavigation('/testimonials')}>Testimonials</span>
+                <span className="Page" onClick={() => handleNavigation('/about')}>About</span>
+                <span className="Page" onClick={() => handleNavigation('/contact')}>Contact</span>
             </div>
             <div className="others">
-            <a href="https://github.com/MuhammadTahirSundhu" target="_blank" rel="noopener noreferrer" className="Navbaricon">
+                <a href="https://github.com/MuhammadTahirSundhu" target="_blank" rel="noopener noreferrer" className="Navbaricon">
                     <FaGithub />
                 </a>
                 <a href="https://www.linkedin.com/in/muhammad-tahir-sundhu/" target="_blank" rel="noopener noreferrer" className="Navbaricon">
@@ -31,10 +36,12 @@ const Navbar: React.FC = () => {
                 <a href="mailto:tahirsundhu8956@gmail.com" className="Navbaricon">
                     <FaGoogle />
                 </a>
-            
+                </div>
+                <div className="menu-icon" onClick={toggleMenu}>
+                    <FaBars />
             </div>
         </nav>
     );
-}
+};
 
 export default Navbar;
